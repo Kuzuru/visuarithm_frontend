5655e6aa86f193d07c07a20439396e9fdeaa395a
Artem Kiselev <kuzuru.dev@gmail.com>
2023-09-02 11:54:20 +0500

    Initial commit

---
+++ components/shared/arrows/RightArrowSvg.vue
@@ -0,0 +1,5 @@
+<template>
+	<svg width="13" height="22" viewBox="0 0 13 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M11.4498 1.74386L2.15381 10.9999L11.4498 20.2559" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+	</svg>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>Default Layout</div>
+	<NuxtPage />
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
